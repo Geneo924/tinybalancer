376650c1bbe6f32a53c9fcb7a627f7f76b795333
geneo924 <genemoore924@gmail.com>
2023-03-03 00:08:58 -0500

    create rp server for relay

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"sync"
@@ -17,4 +18,17 @@ type Backend struct {
 type ServerPool struct {
 	Backends []*Backend
 	current  uint64
+}
+
+
+func main() {
+	u, _ := url.Parse("http://localhost:8080")
+	rp := httputil.NewSingleHostReverseProxy(u)
+
+	handler := http.HandlerFunc(rp.ServeHTTP)
+
+	if err := http.ListenAndServe(":8081",handler); err != nil {
+		panic(err)
+	}
+
 }
