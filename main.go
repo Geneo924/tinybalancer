9bf581d4874d9472b3fb0dec90e3a1b0748ca1eb
geneo924 <genemoore924@gmail.com>
2023-03-03 11:01:36 -0500

    add round robin scheduling

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"sync"
+	"sync/atomic"
 )
 
 type Backend struct {
@@ -15,11 +16,42 @@ type Backend struct {
 	ReverseProxy *httputil.ReverseProxy
 }
 
+func (b *Backend) SetAlive(alive bool) {
+	b.mux.Lock()
+	b.Alive = alive
+	b.mux.Unlock()
+}
+
+func (b *Backend) IsAlive(alive bool) {
+	b.mux.RLock()
+	alive = b.Alive
+	b.mux.RUnlock()
+	return
+}
+
 type ServerPool struct {
-	Backends []*Backend
+	backends []*Backend
 	current  uint64
 }
 
+func (s *ServerPool) NextIndex() int {
+	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.Backends)))
+}
+
+func (s *ServerPool) GetNextPeer() *Backend {
+	next := s.NextIndex()
+	length := len(s.backends) + next
+	for i := next; i < length; i++ {
+		idx := i % len(s.backends)
+		if s.backends[idx].IsAlive() {
+			if i != next {
+				atomic.StoreUint64(&s.current, uint64(idx))
+			}
+			return s.backends[idx]
+		}
+	}
+	return nil
+}
 
 func main() {
 	u, _ := url.Parse("http://localhost:8080")
@@ -27,7 +59,7 @@ func main() {
 
 	handler := http.HandlerFunc(rp.ServeHTTP)
 
-	if err := http.ListenAndServe(":8081",handler); err != nil {
+	if err := http.ListenAndServe(":8081", handler); err != nil {
 		panic(err)
 	}
 
